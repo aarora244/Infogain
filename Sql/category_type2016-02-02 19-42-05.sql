2b26260e5445e5485f2dbc849a1ffed567495095
aarora244 <abhishek2.arora@infogain.com>
2016-02-02 19:44:00 +0530

    Database Sql Script
    Entry for some dummy data

---
+++ Sql/category_type2016-02-02 19-42-05.sql
@@ -0,0 +1,26 @@
+USE eol_prod;
+
+insert into `category_type`(`category_type_id`,`category_type`,`is_system`,`order_no`,`active`,`updated_by`,`created_date`,`modified_date`) values (1,'event_gallery_system',null,null,1,null,'2013-10-15 00:00:00',null);
+insert into `category_type`(`category_type_id`,`category_type`,`is_system`,`order_no`,`active`,`updated_by`,`created_date`,`modified_date`) values (2,'badge_reasons',1,null,1,1,'2013-11-28 00:00:00','2013-11-28 00:00:00');
+insert into `category_type`(`category_type_id`,`category_type`,`is_system`,`order_no`,`active`,`updated_by`,`created_date`,`modified_date`) values (3,'attendance_reasons',1,null,1,8154676,'2014-01-21 14:07:25',null);
+insert into `category_type`(`category_type_id`,`category_type`,`is_system`,`order_no`,`active`,`updated_by`,`created_date`,`modified_date`) values (4,'assignment_type',1,null,1,8154676,'2014-01-21 14:07:43',null);
+insert into `category_type`(`category_type_id`,`category_type`,`is_system`,`order_no`,`active`,`updated_by`,`created_date`,`modified_date`) values (5,'security_question',1,null,1,1,'2014-01-29 10:14:29','2014-01-29 10:14:29');
+insert into `category_type`(`category_type_id`,`category_type`,`is_system`,`order_no`,`active`,`updated_by`,`created_date`,`modified_date`) values (6,'System',null,null,1,8154676,'2014-02-13 12:23:53',null);
+insert into `category_type`(`category_type_id`,`category_type`,`is_system`,`order_no`,`active`,`updated_by`,`created_date`,`modified_date`) values (7,'Gallery_type',null,null,1,8154676,'2014-02-13 12:24:41',null);
+insert into `category_type`(`category_type_id`,`category_type`,`is_system`,`order_no`,`active`,`updated_by`,`created_date`,`modified_date`) values (8,'Event_type',null,null,1,8154676,'2014-02-14 14:28:46',null);
+insert into `category_type`(`category_type_id`,`category_type`,`is_system`,`order_no`,`active`,`updated_by`,`created_date`,`modified_date`) values (9,'School_type',null,null,1,8154676,'2014-02-21 12:33:24',null);
+insert into `category_type`(`category_type_id`,`category_type`,`is_system`,`order_no`,`active`,`updated_by`,`created_date`,`modified_date`) values (10,'city_type',null,null,1,1,'2014-02-25 03:15:02',null);
+insert into `category_type`(`category_type_id`,`category_type`,`is_system`,`order_no`,`active`,`updated_by`,`created_date`,`modified_date`) values (11,'log',null,null,1,1,'2014-03-13 11:15:02',null);
+insert into `category_type`(`category_type_id`,`category_type`,`is_system`,`order_no`,`active`,`updated_by`,`created_date`,`modified_date`) values (12,'Evaluation workflow',1,null,1,8154676,'2014-05-16 21:05:04',null);
+insert into `category_type`(`category_type_id`,`category_type`,`is_system`,`order_no`,`active`,`updated_by`,`created_date`,`modified_date`) values (13,'Competency type',null,null,1,8154676,'2014-06-05 14:02:43',null);
+insert into `category_type`(`category_type_id`,`category_type`,`is_system`,`order_no`,`active`,`updated_by`,`created_date`,`modified_date`) values (14,'Blood Group',null,null,1,1,'2014-06-05 14:02:43',null);
+insert into `category_type`(`category_type_id`,`category_type`,`is_system`,`order_no`,`active`,`updated_by`,`created_date`,`modified_date`) values (15,'School Preference',1,null,1,1,'2014-09-20 00:00:00',null);
+insert into `category_type`(`category_type_id`,`category_type`,`is_system`,`order_no`,`active`,`updated_by`,`created_date`,`modified_date`) values (16,'Feedback Area',null,null,1,30,'2014-10-15 00:00:00','2014-10-15 00:00:00');
+insert into `category_type`(`category_type_id`,`category_type`,`is_system`,`order_no`,`active`,`updated_by`,`created_date`,`modified_date`) values (17,'Feedback Category',null,null,1,39,'2014-10-15 00:00:00','2014-10-15 00:00:00');
+insert into `category_type`(`category_type_id`,`category_type`,`is_system`,`order_no`,`active`,`updated_by`,`created_date`,`modified_date`) values (18,'Feedback Priority',null,null,1,30,'2014-10-15 00:00:00','2014-10-15 00:00:00');
+insert into `category_type`(`category_type_id`,`category_type`,`is_system`,`order_no`,`active`,`updated_by`,`created_date`,`modified_date`) values (19,'School_Types',1,null,1,1,'2015-01-06 00:00:00','2015-01-06 00:00:00');
+insert into `category_type`(`category_type_id`,`category_type`,`is_system`,`order_no`,`active`,`updated_by`,`created_date`,`modified_date`) values (20,'Report Abuse',1,0,1,1,'2015-01-07 00:00:00',null);
+insert into `category_type`(`category_type_id`,`category_type`,`is_system`,`order_no`,`active`,`updated_by`,`created_date`,`modified_date`) values (21,'Product_device_type',1,null,1,1,'2015-09-11 00:00:00','2015-09-11 00:00:00');
+insert into `category_type`(`category_type_id`,`category_type`,`is_system`,`order_no`,`active`,`updated_by`,`created_date`,`modified_date`) values (26,'Product_device_type',1,null,1,1,'2015-09-11 00:00:00','2015-09-11 00:00:00');
+insert into `category_type`(`category_type_id`,`category_type`,`is_system`,`order_no`,`active`,`updated_by`,`created_date`,`modified_date`) values (27,'CCE Templates',1,null,1,null,'2015-09-17 00:00:00','2015-09-17 00:00:00');
+insert into `category_type`(`category_type_id`,`category_type`,`is_system`,`order_no`,`active`,`updated_by`,`created_date`,`modified_date`) values (28,'product_category',1,null,1,1,'2015-12-21 00:00:00',null);
---
+++ Sql/role_master2016-02-02 19-42-38.sql
@@ -0,0 +1,18 @@
+USE eol_prod;
+
+insert into `role_master`(`role_id`,`role_name`,`active`,`updated_by`,`created_date`,`modified_date`) values (1,'Teacher',1,0,'2014-07-07 00:00:00','2013-11-25 00:00:00');
+insert into `role_master`(`role_id`,`role_name`,`active`,`updated_by`,`created_date`,`modified_date`) values (2,'Student',1,0,'2014-07-07 00:00:00','2013-11-25 00:00:00');
+insert into `role_master`(`role_id`,`role_name`,`active`,`updated_by`,`created_date`,`modified_date`) values (3,'School Admin',1,0,'2014-07-07 00:00:00','2013-11-25 00:00:00');
+insert into `role_master`(`role_id`,`role_name`,`active`,`updated_by`,`created_date`,`modified_date`) values (4,'Resource Coordinator',1,0,'2014-07-07 00:00:00','2013-11-25 00:00:00');
+insert into `role_master`(`role_id`,`role_name`,`active`,`updated_by`,`created_date`,`modified_date`) values (5,'Parent',1,0,'2014-07-07 00:00:00','2013-11-25 00:00:00');
+insert into `role_master`(`role_id`,`role_name`,`active`,`updated_by`,`created_date`,`modified_date`) values (6,'Super Admin',1,0,'2014-07-07 00:00:00','2013-11-25 00:00:00');
+insert into `role_master`(`role_id`,`role_name`,`active`,`updated_by`,`created_date`,`modified_date`) values (7,'Web Admin',1,0,'2014-07-07 00:00:00','2013-11-25 00:00:00');
+insert into `role_master`(`role_id`,`role_name`,`active`,`updated_by`,`created_date`,`modified_date`) values (8,'Subject Matter Expert',1,0,'2014-07-07 00:00:00','2013-11-25 00:00:00');
+insert into `role_master`(`role_id`,`role_name`,`active`,`updated_by`,`created_date`,`modified_date`) values (9,'Principal',1,0,'2014-07-07 00:00:00','2013-11-25 00:00:00');
+insert into `role_master`(`role_id`,`role_name`,`active`,`updated_by`,`created_date`,`modified_date`) values (10,'Subject Teacher',1,1,'2014-07-07 00:00:00','2013-11-25 00:00:00');
+insert into `role_master`(`role_id`,`role_name`,`active`,`updated_by`,`created_date`,`modified_date`) values (11,'Group Admin',1,0,'2013-11-25 00:00:00','2013-11-25 00:00:00');
+insert into `role_master`(`role_id`,`role_name`,`active`,`updated_by`,`created_date`,`modified_date`) values (13,'Employee',0,1,'2015-06-18 12:47:00','2015-06-18 12:47:00');
+insert into `role_master`(`role_id`,`role_name`,`active`,`updated_by`,`created_date`,`modified_date`) values (15,'Call Center',0,1,'2015-09-30 00:00:00',null);
+insert into `role_master`(`role_id`,`role_name`,`active`,`updated_by`,`created_date`,`modified_date`) values (16,'Sales Agent',0,1,'2015-12-13 00:00:00',null);
+insert into `role_master`(`role_id`,`role_name`,`active`,`updated_by`,`created_date`,`modified_date`) values (17,'Seller',0,1,'2015-12-13 00:00:00',null);
+insert into `role_master`(`role_id`,`role_name`,`active`,`updated_by`,`created_date`,`modified_date`) values (18,'Engagement Manager',1,1,'2016-01-12 00:00:00',null);
